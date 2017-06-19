e78306039851691b7bf040256bfc1be0fefc0de4
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-19 17:36:10 +0900

    プロジェクトを追加

---
+++ TwitterApplication/TwitterApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterApplication
+//
+//  Created by 村田真矢 on 2017/06/19.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
