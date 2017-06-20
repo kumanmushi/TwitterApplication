dcc8a5402257c77e44738242ad2ae2294b30376f
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 23:30:12 +0900

    パーサーstructを追加

---
+++ TwitterApplication/TwitterApplication/TimelineParser.swift
@@ -0,0 +1,23 @@
+//
+//  TimelineParser.swift
+//  TwitterApplication
+//
+//  Created by 村田真矢 on 2017/06/20.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import Foundation
+
+struct TimelineParser {
+    func parse(data: Data) -> [Tweet] {
+        let serializedData = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        
+        let json = serializedData as! [Any]
+        
+        let timeline: [Tweet] = json.flatMap {
+            Tweet(json: $0)
+        }
+        
+        return timeline
+    }
+}
