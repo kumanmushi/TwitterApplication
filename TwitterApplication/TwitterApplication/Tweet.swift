fbd29fdb9e1f024e68e83fdfdbab0cbe177e11e3
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 23:28:40 +0900

    データモデルにjsonからのinitを作成

--- TwitterApplication/TwitterApplication/Tweet.swift
+++ TwitterApplication/TwitterApplication/Tweet.swift
@@ -12,11 +12,21 @@ struct Tweet {
     
     // Tweetのid
     let id: String
-    
     // Tweetの本文
     let text: String
-    
     // このTweetの主
     let user: User
     
+    init?(json: Any) {
+        guard let dictionary = json as? [String: Any] else { return nil }
+        
+        guard let id = dictionary["id_str"] as? String else { return nil }
+        guard let text = dictionary["text"] as? String else { return nil }
+        guard let userJSON = dictionary["user"] else { return nil }
+        guard let user = User(json: userJSON) else { return nil }
+        
+        self.id = id
+        self.text = text
+        self.user = user
+    }
 }
