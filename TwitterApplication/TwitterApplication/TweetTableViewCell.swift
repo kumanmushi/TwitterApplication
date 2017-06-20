93c28b7226f4bd9887e8a6f821505f52ebafca91
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 23:04:34 +0900

    tweetの値をセットする処理を追加

--- TwitterApplication/TwitterApplication/TweetTableViewCell.swift
+++ TwitterApplication/TwitterApplication/TweetTableViewCell.swift
@@ -11,10 +11,29 @@ import UIKit
 class TweetTableViewCell: UITableViewCell {
     
     @IBOutlet weak var iconImageView: UIImageView!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var screenNameLabel: UILabel!
-    
     @IBOutlet weak var textContentLabel: UILabel!
+    
+    func fill(tweet: Tweet) {
+        // profileImageURLから画像をダウンロードしてくる処理
+        let downloadTask = URLSession.shared.dataTask(with: URL(string: tweet.user.profileImageURL)!) { [weak self] data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // iconImageViewにダウンロードしてきた画像を代入する処理
+                self?.iconImageView.image = UIImage(data: data!)
+            }
+        }
+        downloadTask.resume()
+        
+        // tweetから値を取り出して、UIにセットする
+        nameLabel.text = tweet.user.name
+        textContentLabel.text = tweet.text
+        // screenNameには "@" が含まれていないので、頭に "@" を入れてあげる必要がある
+        screenNameLabel.text = "@" + tweet.user.screenName
+    }
 }
