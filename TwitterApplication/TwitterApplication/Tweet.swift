c30ecae3b7828bbce30c850bdbda652adf9b6c0f
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 21:54:42 +0900

    Tweetモデルを追加

--- TwitterApplication/TwitterApplication/Tweet.swift
+++ TwitterApplication/TwitterApplication/Tweet.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+struct Tweet {
+    
+    // Tweetのid
+    let id: String
+    
+    // Tweetの本文
+    let text: String
+    
+    // このTweetの主
+    let user: User
+    
+}
