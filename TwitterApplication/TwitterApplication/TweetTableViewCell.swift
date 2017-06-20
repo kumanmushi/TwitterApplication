8073890abd17f094f2ce71c41e7c6659c2c42c27
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 22:42:42 +0900

    TweetTableViewCellを追加

---
+++ TwitterApplication/TwitterApplication/TweetTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterApplication
+//
+//  Created by 村田真矢 on 2017/06/20.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+}
