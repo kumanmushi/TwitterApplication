8905ae9328ea1164909b9a03f56b2d3d5e2699f5
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-06-20 21:54:57 +0900

    Userモデルを追加

--- TwitterApplication/TwitterApplication/User.swift
+++ TwitterApplication/TwitterApplication/User.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct User {
+    
+    // ユーザーのid
+    let id: String
+    
+    // (@)ktanaka117
+    let screenName: String
+    
+    // ダンボー田中
+    let name: String
+    
+    // プロフィール画像URL
+    let profileImageURL: String
+    
+}
